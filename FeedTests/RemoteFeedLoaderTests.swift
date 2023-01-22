3a39a0a751b15d4db47a44a3f32543dfcc8a530b
Serhii Kopanitskyi <sergey.kopanitskyi@branderstudio.com>
2023-01-22 12:15:29 +0200

    Create http client spy, move logic to it

--- FeedTests/RemoteFeedLoaderTests.swift
+++ FeedTests/RemoteFeedLoaderTests.swift
@@ -9,30 +9,37 @@ import XCTest
 
 class RemoteFeedLoader {
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "https://google.com")
+        HTTPClient.shared.get(from: URL(string: "https://google.com")!)
     }
 }
 
 class HTTPClient {
-    
-    static let shared = HTTPClient()
-    
-    private init() { }
-    
+    static var shared = HTTPClient()
+        
+    public func get(from url: URL) { }
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    public override func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 class FeedTests: XCTestCase {
     
     func test_doesnt_fetch_request() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_request() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteFeedLoader()
         
         sut.load()
