0118d718ab5874fab4cb557e38cfb2a43e40d529
Serhii Kopanitskyi <sergey.kopanitskyi@branderstudio.com>
2023-01-22 18:40:10 +0200

    Refactor feed loader tests

--- FeedTests/RemoteFeedLoaderTests.swift
+++ FeedTests/RemoteFeedLoaderTests.swift
@@ -14,39 +14,48 @@ protocol HTTPClient {
 class RemoteFeedLoader {
     
     let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient) {
+    init(client: HTTPClient, url: URL) {
         self.client = client
+        self.url = url
     }
     
     func load() {
-        client.get(from: URL(string: "https://google.com")!)
-    }
-}
-
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
-    
-    func get(from url: URL) {
-        requestedURL = url
+        client.get(from: url)
     }
 }
 
 class FeedTests: XCTestCase {
     
     func test_doesnt_fetch_request() {
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        let url = URL(string: "https://google.com")!
+        let (client, _) = makeSUT(url: url)
         
         XCTAssertNil(client.requestedURL)
+        XCTAssertNotEqual(url, client.requestedURL)
     }
     
     func test_load_request() {
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
-        
+        let url = URL(string: "https://google.com")!
+        let (client, sut) = makeSUT(url: url)
         sut.load()
         
         XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(url, client.requestedURL)
+    }
+    
+    private func makeSUT(url: URL) -> (client: HTTPClientSpy, feedLoader: RemoteFeedLoader) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client, url: url)
+        return (client, sut)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
     }
 }
