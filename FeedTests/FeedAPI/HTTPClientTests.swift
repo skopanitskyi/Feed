2b78ccfb0d7ecd374aeaa4ca09c98cb09bf1bdc9
Serhii Kopanitskyi <sergey.kopanitskyi@branderstudio.com>
2023-01-26 23:20:15 +0200

    Add test for check fetched result on error

--- FeedTests/FeedAPI/HTTPClientTests.swift
+++ FeedTests/FeedAPI/HTTPClientTests.swift
@@ -15,33 +15,86 @@ class URLSessionHTTPClient {
         self.session = session
     }
     
-    func fetch(from url: URL) {
-        session.dataTask(with: url) { _, _, _ in }
+    func fetch(from url: URL, completion: @escaping (HTTPClientResponse) -> Void) {
+        session.dataTask(with: url) { _, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+        }.resume()
     }
     
 }
 
 class HTTPClientTests: XCTestCase {
     
-    func test_fetchFromURL_retrunDataTaskWithURL() {
+    func test_fetchFromURL_callResumeOnce() {
         let url = URL(string: "https://google.com")!
         let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
         let sut = URLSessionHTTPClient(session: session)
+        session.stub(url, task)
         
-        sut.fetch(from: url)
+        sut.fetch(from: url) { _ in }
+        XCTAssertEqual(task.resumeCalledCount, 1)
+    }
+    
+    func test_fetchFromURL_failsOnRequest() {
         
-        XCTAssertEqual(session.caputedURLs, [url])
+        let url = URL(string: "https://google.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        let expextedError = NSError(domain: "test error", code: 1)
+        session.stub(url, task, error: expextedError)
+        
+        let exp = expectation(description: "wait call completion")
+        
+        sut.fetch(from: url) { result in
+            switch result {
+            case .success:
+                XCTFail()
+            case .failure(let error as NSError):
+                XCTAssertEqual(expextedError, error)
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1)
     }
     
     private class URLSessionSpy: URLSession {
         var caputedURLs: [URL] = []
+        private var stubs: [URL: Stub] = [:]
+                
+        func stub(_ url: URL, _ dataTask: URLSessionDataTask = URLSessionFakeDataTask(), error: Error? = nil) {
+            stubs[url] = .init(dataTask: dataTask, error: error)
+        }
         
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             caputedURLs.append(url)
-            return URLSessionFakeDataTask()
+            guard let stub = stubs[url] else {
+                fatalError("didn't faind stub")
+            }
+            completionHandler(nil, nil, stub.error)
+            return stub.dataTask
         }
     }
     
-    private class URLSessionFakeDataTask: URLSessionDataTask { }
+    private struct Stub {
+        let dataTask: URLSessionDataTask
+        let error: Error?
+    }
+    
+    private class URLSessionFakeDataTask: URLSessionDataTask {
+        override func resume() { }
+    }
+    
+    private class URLSessionDataTaskSpy: URLSessionDataTask {
+        var resumeCalledCount: Int = .zero
+        
+        override func resume() {
+            resumeCalledCount += 1
+        }
+    }
     
 }
